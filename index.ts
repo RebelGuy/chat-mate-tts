fa1926b64fb7f9b94ea5f38b77fb829d52ae7501
Rebel Guy <rebelguysminecraft@gmail.com>
2025-04-27 20:29:53 +1000

    Added random voice

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import { spawn } from 'child_process'
 import { Readable } from 'stream'
 import { rm } from 'fs/promises'
 
-// const SERVER_URL = 'https://chat-mate-prod.azurewebsites.net'
-const SERVER_URL = 'http://localhost:3010'
+const SERVER_URL = 'https://chat-mate-prod.azurewebsites.net'
+// const SERVER_URL = 'http://localhost:3010'
 
 const ZONOS_URL = 'http://localhost:8001'
 
@@ -13,7 +13,8 @@ const COQUI_URL = 'http://localhost:5002'
 const ZONOS_MODEL = 'transformer'
 // const ZONOS_MODEL = 'hybrid'
 
-const COQUI_SPEAKER_ID = 'p230'
+// const COQUI_SPEAKER_ID = 'p263'
+const COQUI_SPEAKER_IDS = ["ED","p225","p226","p227","p228","p229","p230","p231","p232","p233","p234","p236","p237","p238","p239","p240","p241","p243","p244","p245","p246","p247","p248","p249","p250","p251","p252","p253","p254","p255","p256","p257","p258","p259","p260","p261","p262","p263","p264","p265","p266","p267","p268","p269","p270","p271","p272","p273","p274","p275","p276","p277","p278","p279","p280","p281","p282","p283","p284","p285","p286","p287","p288","p292","p293","p294","p295","p297","p298","p299","p300","p301","p302","p303","p304","p305","p306","p307","p308","p310","p311","p312","p313","p314","p316","p317","p318","p323","p326","p329","p330","p333","p334","p335","p336","p339","p340","p341","p343","p345","p347","p351","p360","p361","p362","p363","p364","p374","p376"]
 
 const AUDIO_VOLUME = 100 // between 0 and 100
 
@@ -149,7 +150,8 @@ async function playAudioFromTextQuick (str: string) {
 
 // outputs the file name
 async function generateSpeechQuick (str: string): Promise<ArrayBuffer> {
-  const response = await fetch(`${COQUI_URL}/api/tts?text=${encodeURI(str)}&speaker_id=${COQUI_SPEAKER_ID}`, { method: 'GET' })
+  const speaker = COQUI_SPEAKER_IDS[Math.floor(Math.random() * COQUI_SPEAKER_IDS.length)]
+  const response = await fetch(`${COQUI_URL}/api/tts?text=${encodeURI(str)}&speaker_id=${speaker}`, { method: 'GET' })
 
   if (!response.ok) {
     throw new Error(`Failed to generate speech. Code ${response.status}: ${await response.text()}`)
